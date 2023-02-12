41978801306a21d576230cd877c3f4c84d98c593
unknown <developer.mariano@gmail.com>
2023-02-12 14:57:37 -0300

    update savecolleg.js - insert loop function e condictional

--- savecollege.js
+++ savecollege.js
@@ -40,3 +40,30 @@ let aluno = [
         matematica:9,
         },
     ]
+//Inicio - Ex1 - dados dos alunos
+console.log('::Ex1 - dados dos alunos')
+/*aluno.forEach(exibirAlunos);
+function exibirAlunos(item){
+    console.log(item.nome,item.altura,item.idade,item.sexo,item.sala);
+}*/
+for (var i = 0; i < aluno.length; i++) {
+    console.log(aluno[i].nome,aluno[i].altura,aluno[i].idade,aluno[i].sexo,aluno[i].sala);
+    // more statements
+ }
+//Fim - Ex1 - dados dos alunos
+//Inicio - Ex2 - média dos alunos
+console.log('::Ex2 - média dos alunos')
+let media=0;
+let cont=0;
+while (cont < aluno.length) {
+    media += aluno[cont].matematica;
+    cont++;
+}
+console.log(`Média da nota de matematica dos alunos: ${media}`)
+//Inicio - Ex3 - alunos aprovados
+console.log('::Ex3 - alunos aprovados')
+for (var i=0; i < aluno.length; i++) {
+    aluno[i].matematica >= 7 ? console.log(`${aluno[i].nome} APROVADO`) : console.log(`${aluno[i].nome} REPROVADO`)
+}
+
+//Fim - Ex3 - alunos aprovados
